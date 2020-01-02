3a056035d6067f0afad858903a98b21cdad7db87
brenshanthakumar <sehmim@Sehmims-MacBook-Pro.local>
2020-01-01 19:33:01 -0500

    initial commit

---
+++ src/api/FirebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDSzGJZBvuGEXcQxztSsA7jUhvzuKKHBzU",
+    authDomain: "react-native-app-4fe0f.firebaseapp.com",
+    databaseURL: "https://react-native-app-4fe0f.firebaseio.com",
+    projectId: "react-native-app-4fe0f",
+    storageBucket: "react-native-app-4fe0f.appspot.com",
+    messagingSenderId: "800939574379",
+    appId: "1:800939574379:web:9bed43eee0bb12f9e19d2e",
+    measurementId: "G-WXNJ964FFJ"
+};
